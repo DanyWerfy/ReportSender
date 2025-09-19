b9fefb35efe9576024ed76ddc67876a4230b8528
Dany Diab <dany@werfy.com>
2025-09-19 18:28:34 -0400

    started work on csvNormalizer

---
+++ scripts/NormalizeCsv.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import itertools
+import re
+
+def main():
+    csvPath = "../data/HOTEL CO OWNERS UNIT AND EMAIL ID(Information from Dec2023).csv"
+    outputPath = "../data/coOwnerEmails.csv"
+
+    with open(csvPath, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f=f, delimiter=";")
+        emails = []
+        for row in reader:
+            currEmail = row["EMAIL ADDRESS"]
+            emailNormal = re.sub("\r*\s*", "",currEmail)
+            emailSplit = emailNormal.split(",")
+            emails.append(emailSplit)
+
+        flatEmails = list(itertools.chain.from_iterable(emails))
+        print(flatEmails)
+
+if __name__ == "__main__":
+    main()
